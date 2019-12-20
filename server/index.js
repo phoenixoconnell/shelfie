4de818cea7f04d13c655d6c7344d4cb5d9eda6de
Phoenix Oconnell <phoenix.oconnell@gmail.com>
2019-12-20 16:14:43 -0600

    hooks and buttons

--- server/index.js
+++ server/index.js
@@ -4,6 +4,13 @@ const app = express();
 const massive = require('massive');
 const controller = require('./controller')
 
+const { SERVER_PORT, DATABASE_STRING } = process.env;
+
+massive(DATABASE_STRING).then(db => {
+    app.set('db', db);
+    console.log("Database Connected");
+});
+
 app.listen(5150, () => {
     console.log('Listening on port 5150')
 });
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,11 +1,49 @@
 import React, { Component } from "react";
 
 export default class Form extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            name: '',
+            price: 0,
+            imgurl: ''
+        };
+
+        this.handleCancel = this.handleCancel.bind(this);
+        
+    }
+
+
+    handleNameChange(value) {
+        this.setState({ name: value });
+    }
+
+    handlePriceChange(value) {
+        this.setState({ price: value });
+    }
+
+    handleImgChange(value) {
+        this.setState({ imgurl: value});
+    }
+
+    handleCancel() {
+        this.setState({
+            name: '',
+            price: 0,
+            imgurl: ''
+        });
+    }
+    
     render() {
         return (
             <div>
-                <p>Form</p>
-            </div>
+                <input onChange={e => this.handleImgChange(e.target.value)} type="text" />
+                <input onChange={e => this.handleNameChange(e.target.value)} type="text" />
+                <input onChange={e => this.handlePriceChange(e.target.value)} type="text" />
+                <button onClick={this.handleCancel}>Cancel</button>
+                <button>Add to Inventory</button>
+                </div>
         );
     }
 }
